d289d353f91669b69537733f1a72281aba110ed2
VadymCov <vadymkravtcsov@gmail.com>
2025-07-24 22:22:05 +0300

    added main menu with build-in button

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio 
 import sqlite3
 from aiogram import Bot,Dispatcher
-from aiogram.types import Message
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command 
 from aiogram.fsm.storage.memory import MemoryStorage # connect the module state storage in RAM
 from aiogram.fsm.state import State, StatesGroup
@@ -16,6 +16,14 @@ class TaskStates(StatesGroup): # Condition for FSM
     waiting_for_task = State()
     waiting_for_id = State()
 
+def get_main_menu(): # main menu with build-in button
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="add task", callback_data="add_task")],
+    [InlineKeyboardButton(text="delete task", callback_data="delete_task")],
+    [InlineKeyboardButton(text="completed", callback_data="completed")],
+    [InlineKeyboardButton(text="list task", callback_data="list_task")],
+    ])
+
 def init_db(): # Create database
     con = sqlite3.connect('user_todo_db')
     cur = con.cursor()
@@ -24,7 +32,7 @@ def init_db(): # Create database
                 id INTEGER PRIMARY KEY,
                 title TEXT
                 done BOOLEAN
-                )e
+                )
                 ''')
     con.commit()
     con.close()
