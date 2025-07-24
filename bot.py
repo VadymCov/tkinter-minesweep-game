706775991e1a334b1a9fb38499f9057da721b317
VadymCov <vadymkravtcsov@gmail.com>
2025-07-24 18:27:29 +0300

    # connect the module state storage in RAM | # in-memory state store |  # bind storage to the manager

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import asyncio 
 import sqlite3
 from aiogram import Bot,Dispatcher
+from aiogram.types import Message
+from aiogram.filters import Command 
+from aiogram.fsm.storage.memory import MemoryStorage # connect the module state storage in RAM
 from config import YOUR_TOKEN # Important: config.py is added to .gitignore to prevent token leakage.
 
 bot = Bot(token=YOUR_TOKEN) # Initialize the bot using the token from config.py.
-dp = Dispatcher 
+storage = MemoryStorage() # in-memory state store
+dp = Dispatcher(storage=storage) # bind storage to the manager
+
 
 # Create database
 def init_db():
